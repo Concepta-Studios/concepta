2aa5bd1fb3f10bd0e1b9aca4547a8e24a8576a01
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2024-05-02 23:44:38 +0200

    feat: add Plus icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Logo from './Logo';
+import PlusIcon from '../icons/PlusIcon';
 
 function Header() {
   return (
     <nav className="absolute w-full p-10">
       <div className="font-['Campton'] font-light flex justify-between items-center">
-        <div className="">
+        <div className="cursor-pointer">
           <Logo />
         </div>
         <div className="text-white flex text-xl">
-          <div className="mr-16">Contact</div>
-          <div>Menu</div>
+          <div className="mr-16 cursor-pointer">Contact</div>
+          <div className="flex cursor-pointer">
+            <span className="mr-1">Menu</span>
+            <PlusIcon />
+          </div>
         </div>
       </div>
     </nav>
