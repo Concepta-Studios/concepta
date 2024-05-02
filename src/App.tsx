a624dd9be087d92a261523687bcab85f18f17352
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2024-05-03 00:01:35 +0200

    feat: add ArrowLongRightIcon and update texts on the main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './index.css';
 import Video from './video/hero.mp4';
+import ArrowLongRightIcon from './icons/ArrowLongRightIcon';
 
 function App() {
   return (
@@ -10,10 +11,16 @@ function App() {
         <source src={Video} type="video/mp4" />
       </video>
       <Header />
-      <h1 className="text-8xl absolute text-white/75 uppercase top-1/2 w-full text-center font-['AvenirBlack']">
-        Digital solutions
-      </h1>
-      <div>Who needs a rocket?</div>
+      <div className="absolute top-1/2 font-['AvenirBlack'] left-1/2 -translate-x-2/4">
+        <h1 className="text-8xl uppercase text-white/75">Digital solutions</h1>
+        <div className="text-white text-2xl font-['Campton'] font-medium mb-6">
+          Who needs a rocket?
+        </div>
+        <div className="text-white flex text-xl font-['Campton'] font-medium cursor-pointer">
+          <span className="mr-1">Our services</span>
+          <ArrowLongRightIcon />
+        </div>
+      </div>
     </div>
   );
 }
