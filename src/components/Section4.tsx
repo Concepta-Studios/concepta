3c2eedcca9eb733168918c4fe347cd82c5c8b471
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2024-05-08 09:27:10 +0200

    feat: update sections 4 and 5

--- src/components/Section4.tsx
+++ src/components/Section4.tsx
@@ -1,11 +1,24 @@
+import ArrowLongRightIcon from '../icons/ArrowLongRightIcon';
+import ChameleonPic from '../pics/chameleon.png';
+
 function Section4() {
   return (
-    <div className="bg-black text-white pb-32 px-10">
-      <div className="max-w-screen-2xl m-auto">
-        <h1 className="font-['AvenirBlack'] text-6xl pb-8 text-center">
+    <div className="bg-black text-white pb-32 flex">
+      <div className="w-2/5 pl-20">
+        <h1 className="font-['AvenirBlack'] text-6xl pb-8">
           Lorem ipsum dolor
         </h1>
+        <div className="text-[#87868B] font-['LexendDeca'] font-light text-xl mb-12">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu
+          justo id nisi dignissim. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Maecenas eu justo id nisi dignissim.
+        </div>
+        <a className="flex w-1/2 text-[#05C9C2] text-xl" href="/lorem-ipsum">
+          <span className="mr-2">Lorem ipsum</span>
+          <ArrowLongRightIcon />
+        </a>
       </div>
+      <img className="w-3/5" src={ChameleonPic} alt="Chameleon" />
     </div>
   );
 }
--- src/components/Section5.tsx
+++ src/components/Section5.tsx
@@ -1,10 +1,25 @@
+import ArrowLongRightIcon from '../icons/ArrowLongRightIcon';
+import DigitalSolutionsPic from '../pics/digital_solutions_ugly_screenshot.png';
+
 function Section5() {
   return (
-    <div className="bg-black text-white pb-32 px-10">
-      <div className="max-w-screen-2xl m-auto">
-        <h1 className="font-['AvenirBlack'] text-6xl pb-8 text-center">
-          Aenean urna est
-        </h1>
+    <div className="bg-black text-white pb-32 flex">
+      <img
+        className="w-1/2 mr-20"
+        src={DigitalSolutionsPic}
+        alt="Digital solutions"
+      />
+      <div className="w-1/2 pr-20">
+        <h1 className="font-['AvenirBlack'] text-6xl pb-8">Aenean urna est</h1>
+        <div className="text-[#87868B] font-['LexendDeca'] font-light text-xl mb-12">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu
+          justo id nisi dignissim. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Maecenas eu justo id nisi dignissim.
+        </div>
+        <a className="flex w-1/2 text-[#05C9C2] text-xl" href="/lorem-ipsum">
+          <span className="mr-2">Lorem ipsum</span>
+          <ArrowLongRightIcon />
+        </a>
       </div>
     </div>
   );
