277884e4777713918664dcc5283c9b7032cc410e
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2024-05-12 14:48:43 +0200

    refactor: carousel with react-slick

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,63 +1,47 @@
-import { useEffect, useState } from 'react';
 import ArrowLongRightIcon from '../icons/ArrowLongRightIcon';
 import Slider from 'react-slick';
 import './Carousel.css';
 
 function Carousel() {
   const data = ['1', '2', '3'];
-  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const carouselInfiniteScroll = () => {
-    if (currentIndex === data.length - 1) {
-      return setCurrentIndex(0);
-    }
-
-    return setCurrentIndex(currentIndex + 1);
-  };
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     carouselInfiniteScroll();
-  //   }, 5000);
-  //   return () => clearInterval(interval);
-  // });
   const settings = {
-    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    centerModer: true,
   };
 
   return (
-    <Slider {...settings}>
-      {data.map((_, index) => (
-        <div key={index}>
-          <div className="flex">
-            <div className="w-1/2 mr-16 bg-white rounded-3xl h-[300px]"></div>
-            <div className="w-1/2 font-['LexendDeca']">
-              <h2 className="text-4xl font-bold mb-10">Lorem ipsum dolor</h2>
-              <div className="text-xl font-bold mb-10">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas eu justo id nisi dignissim. Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Maecenas eu justo id nisi
-                dignissim.
+    <div className="slider-container">
+      <Slider {...settings}>
+        {data.map((_, index) => (
+          <div key={index}>
+            <div className="flex p-20">
+              <div className="w-1/2 mr-16 bg-white rounded-3xl h-[400px]"></div>
+              <div className="w-1/2 font-['LexendDeca']">
+                <h2 className="text-4xl font-bold mb-10">Lorem ipsum dolor</h2>
+                <div className="text-xl font-bold mb-10">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Maecenas eu justo id nisi dignissim. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit. Maecenas eu justo id nisi
+                  dignissim.
+                </div>
+                <a
+                  className="flex w-1/2 text-[#05C9C2] text-xl"
+                  href="https://example.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="mr-2">Visit homepage</span>
+                  <ArrowLongRightIcon />
+                </a>
               </div>
-              <a
-                className="flex w-1/2 text-[#05C9C2] text-xl"
-                href="https://example.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span className="mr-2">Visit homepage</span>
-                <ArrowLongRightIcon />
-              </a>
             </div>
           </div>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </Slider>
+    </div>
   );
 }
 
